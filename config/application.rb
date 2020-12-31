8d18a398762e449b86567caf32740da22e5b1169
Mathieu Magnin <mathieu.magnin@gmail.com>
2020-12-31 17:06:20 +0100

    Run update task 'bin/rails app:update'

--- config/application.rb
+++ config/application.rb
@@ -12,16 +12,12 @@ module MotionDemos
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
-
-    config.action_mailer.default_url_options = {
-      host: "localhost",
-      port: 3000
-    }
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
